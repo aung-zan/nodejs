d9907deac7afbea45dbd9c15e3eb71362e445608
Aung Min Zan <nightkiller10@gmail.com>
2025-02-24 16:09:11 +0700

    add routes, html pages and static css

--- node-complete-guide/app.js
+++ node-complete-guide/app.js
@@ -1,18 +1,26 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use((req, res, next) => {
-  if (req.url === '/favicon.ico') {
-    res.status(204).end();
-    return;
-  }
-  console.log("First Middleware");
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/favicon.ico", (req, res, next) => {
+  res.status(204).end();
 });
 
+app.use('/admin/', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.send("<h1>Hello From The Express!</h1>")
+  res.status(404)
+    .sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
