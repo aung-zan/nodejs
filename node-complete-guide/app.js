72f7652231ae74243304ca7985b48e0fe8e41254
Aung Min Zan <nightkiller10@gmail.com>
2025-02-24 11:25:49 +0700

    add .gitignore and express

--- node-complete-guide/app.js
+++ node-complete-guide/app.js
@@ -1,43 +1,18 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const app = express();
 
-  if (url === '/') {
-    res.write("<html>");
-    res.write("<head><title>Enter a message</title></head>");
-    res.write("<body>");
-    res.write("<form action='/message' method='POST'>");
-    res.write("<input type='text' name='message'>");
-    res.write("<button type='submit'>Submit</button>")
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-
-    return res.end();
+app.use((req, res, next) => {
+  if (req.url === '/favicon.ico') {
+    res.status(204).end();
+    return;
   }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      const data = parseBody.split("=");
-      const [, text] = [...data];
-      fs.writeFileSync("test.txt", text);
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-
-    return res.end();
-  }
+  console.log("First Middleware");
+  next();
 });
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res.send("<h1>Hello From The Express!</h1>")
+});
+
+app.listen(3000);
