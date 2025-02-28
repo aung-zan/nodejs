4f04c015854d163dbb7b38b53e6c12d9effd4b7e
Aung Min Zan <nightkiller10@gmail.com>
2025-02-28 15:54:42 +0700

    add ShopController and update ProductController in user

--- node-complete-guide/app/Controllers/User/ProductController.js
+++ node-complete-guide/app/Controllers/User/ProductController.js
@@ -6,12 +6,30 @@ exports.list = async (req, res, next) => {
     const products = await product.all();
 
     res.render("user/product/list.ejs", {
-      title: "Product List",
-      path: "/",
+      title: "Products List",
+      path: "/products",
       products: products,
     });
   } catch (error) {
     console.error("Error fetching products:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+exports.details = async (req, res, next) => {
+  try {
+    const id = Number(req.params.productId);
+
+    const p = new Product();
+    const product = await p.getById(id);
+
+    res.render("user/product/details.ejs", {
+      title: "Product Details",
+      path: "/products",
+      product: product
+    });
+  } catch (error) {
+    console.error("Error fetching product:", error);
     res.status(500).send("Internal Server Error");
   }
 }
--- node-complete-guide/routes/user.js
+++ node-complete-guide/routes/user.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
 const ProductController = require("../app/Controllers/User/ProductController");
+const ShopController = require("../app/Controllers/User/ShopController");
 
 const routes = express.Router();
 
-routes.get('/', ProductController.list);
+routes.get('/', ShopController.list);
+
+routes.get('/products', ProductController.list);
+routes.get('/product/:productId', ProductController.details);
+
+routes.get('/cart');
+routes.post('/cart');
+routes.post('/cart/delete');
+
+routes.get('/orders');
+
+routes.get('/checkout');
 
 module.exports = routes;
